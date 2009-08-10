08e94325acccf7ab4bb0b7289decfd6704b30b03
Espen A. Kristiansen <post@espenak.net>
2009-08-10 10:03:41 +0200

    Added HelloWorld.

---
+++ onsdag/HelloWorld.java
@@ -0,0 +1,17 @@
+public class HelloWorld {
+	public static void main(String[] args) {
+
+		// Write the text "Hello World!" to the screen
+		System.out.println("Hello World!");
+
+		// Calculate 2 + 2 and store the result in the varable four.
+		int four = 2 + 2;
+
+		// Write the result of the calculation to the screen.
+		System.out.println("2+2 = " + four);
+
+		// Get name from the input to the program
+		String name = args[0];
+		System.out.println("Your name is: " + name);
+	}
+}
